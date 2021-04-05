949aac1a2bc6b7361816e9c2a5904432b73bf48f
guitar <keeta254211@gmail.com>
2021-04-05 14:25:14 +0700

    change path that save image

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::post('/parse2Text', function (Request $request) {
         $person["birthday"] = $array[1];
         $valid3 = true;
     }
-    $path = $request->file("image")->storeAs("base64", "imageCard.jpg");
+    $path = $request->file("image")->storeAs("public/base64", "imageCard.jpg");
 
     // $request->file('image')->storeAs();
     // $base64 = base64_encode(file_get_contents($request->file('image')));
